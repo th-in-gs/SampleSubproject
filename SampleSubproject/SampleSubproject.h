ea15b0a9e7d0be0b15a2d83b0a3ffe24e9ea6dbc
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2012-04-22 10:08:50 +0100

    Initial Commit

---
+++ SampleSubproject/SampleSubproject.h
@@ -0,0 +1,13 @@
+//
+//  SampleSubproject.h
+//  SampleSubproject
+//
+//  Created by James Montgomerie on 22/04/2012.
+//  Copyright (c) 2012 Things Made Out Of Other Things. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleSubproject : NSObject
+
+@end
---
+++ SampleSubproject/SampleSubproject.m
@@ -0,0 +1,13 @@
+//
+//  SampleSubproject.m
+//  SampleSubproject
+//
+//  Created by James Montgomerie on 22/04/2012.
+//  Copyright (c) 2012 Things Made Out Of Other Things. All rights reserved.
+//
+
+#import "SampleSubproject.h"
+
+@implementation SampleSubproject
+
+@end
